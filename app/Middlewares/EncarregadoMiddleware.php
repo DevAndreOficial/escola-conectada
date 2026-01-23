11793b1361f06a20df3193628b60849b7ef6525b
= <=>
2026-01-23 16:53:58 +0100

    adicionando a midleware do encarregado

---
+++ app/Middlewares/EncarregadoMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Middlewares;
+
+use Pecee\Http\Middleware\IMiddleware;
+use Pecee\Http\Request;
+
+class EncarregadoMiddleware implements IMiddleware
+{
+    public function handle(Request $request): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (
+            empty($_SESSION['user']) ||
+            $_SESSION['user']['perfil'] !== 'aluno'
+        ) {
+            header('Location: ' . URL_DESENVOLVIMENTO . '/encarregado');
+            exit;
+        }
+    }
+}
